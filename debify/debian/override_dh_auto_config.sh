5be813f3b1a15db386703212c5da87dab7907856
Julian Brown <julian.brown@cpanel.net>
2021-06-28 15:46:42 -0500

    this corrects the apr issue

--- debify/debian/override_dh_auto_config.sh
+++ debify/debian/override_dh_auto_config.sh
@@ -2,7 +2,21 @@
 
 source debian/vars.sh
 
+set -x
+
 : Building $name $version-$release $_arch $ns_name-mmn = $_httpd_mmn
+
+# pulled from apr-util
+mkdir -p config
+cp $ea_apr_config config/apr-1-config
+cp $ea_apr_config config/apr-config
+cp /usr/share/pkgconfig/ea-apr16-1.pc config/apr-1.pc
+cp /usr/share/pkgconfig/ea-apr16-util-1.pc config/apr-util-1.pc
+cp /usr/share/pkgconfig/ea-apr16-1.pc config
+cp /usr/share/pkgconfig/ea-apr16-util-1.pc config
+
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:`pwd`/config"
+touch configure
 
 $_httpd_apxs -l cap -c $module_name.c
 mv .libs/$module_name.so .
